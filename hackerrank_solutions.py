397e934ffff76e440df5b8a0903129bef22d49c4
Emdad Hossain <emdadhossain25@users.noreply.github.com>
2026-01-13 19:41:49 +0600

    day3: problem Runner up score

--- hackerrank_solutions.py
+++ hackerrank_solutions.py
@@ -59,3 +59,12 @@ if __name__ == '__main__':
     integer_list = map(int, raw_input().split())
     t = tuple(integer_list)
     print (hash(t))
+
+
+# problem Runner-Up Score
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr = list(set(arr))
+    arr.sort()
+    print(arr[-2])
