a49a7df0d85523fa49fed23b8e6e06f26018a8cf
Emdad Hossain <emdadhossain25@users.noreply.github.com>
2026-01-14 19:38:38 +0600

    day5: Scatter plot using matplotlib

--- python-ml-projects/day5_matplotlib_basics.py
+++ python-ml-projects/day5_matplotlib_basics.py
@@ -32,3 +32,38 @@ plt.grid(True,alpha = 0.3)
 plt.savefig('plots/01_line_plot.png', dpi =300, bbox_inches ='tight')
 plt.close()
 print("✅ Saved: plots/01_line_plot.png")
+
+
+
+# ============================================
+# 2. SCATTER PLOT
+# ============================================
+
+print("2. Creating a scatter plot...")
+
+#Random data with correlation
+np.random.seed(42)
+x = np.random.rand(100)
+y = 2 * x + np.random.random(100) * 0.5
+
+plt.figure(figsize=(10,6))
+plt.scatter(x,y, alpha =0.6, c=y, cmap = 'viridis', s = 50, edgecolor = 'black')
+plt.colorbar(label = 'Y Value')
+
+
+plt.title('Scatter Plot with Correlation', fontsize = 16, fontweight = 'bold')
+plt.xlabel('X values', fontsize = 12)
+plt.ylabel('Y values', fontsize = 12)
+plt.grid(True, alpha = 0.3)
+
+
+plt.savefig('plots/02_scatter_plot.png', dpi = 300, bbox_inches ='tight')
+plt.close()
+print("✅ Saved: plots/02_scatter_plot.png")
+
+
+
+
+
+
+
